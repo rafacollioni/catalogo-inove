e71189758ab30a330a36579609315f8c945c3dc8
Rafael Collioni <rafa.collioni@gmail.com>
2026-02-19 21:18:16 -0300

    fix: correct syntax in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
-webpack: (config) => {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    "fs": false,
-    "path": false,
-    "os": false,
-  };
-  return config;
-},
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "fs": false,
+      "path": false,
+      "os": false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
